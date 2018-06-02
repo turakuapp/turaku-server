7c8769b43d7424804e3af70a480a1ca8d805c598
Hari Gopal <mail@harigopal.in>
2018-06-02 18:26:51 +0530

    Seed a team for test user

--- db/seeds/development/helper.rb
+++ db/seeds/development/helper.rb
@@ -1,4 +1,5 @@
 class Seed
+  # TODO: Seed.hash_password uses SHA256 for hashing. Replace with PBKDF2 or better.
   def self.hash_password(secret)
     authentication_salt = SecureRandom.hex(32)
     encryption_salt = SecureRandom.hex(32)
@@ -7,6 +8,27 @@ class Seed
       password: Digest::SHA2.hexdigest(secret + authentication_salt),
       authentication_salt: authentication_salt,
       encryption_salt: encryption_salt
+    }
+  end
+
+  # TODO: Seed.encrypted_password uses SHA256 for hashing. Replace with PBKDF2 or better.
+  def self.encrypted_password(encryption_salt)
+    # Create a random 256-bit password as the shared password for team.
+    password = SecureRandom.base64(32)
+
+    # Create a key to encrypt the team password (INSECURE) by hashing the user's password with the encryption salt.
+    key = Digest::SHA2.base64digest("password#{encryption_salt}")
+
+    # Encrypt the team password with the key and a random IV.
+    cipher = OpenSSL::Cipher.new 'AES-256-CBC'
+    cipher.encrypt
+    iv = cipher.random_iv
+    cipher.key= Base64.decode64(key)
+    encrypted = cipher.update(password) + cipher.final
+
+    {
+      ciphertext: Base64.encode64(encrypted),
+      iv: Base64.encode64(iv)
     }
   end
 end
