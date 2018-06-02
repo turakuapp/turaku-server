d8178fcc2d452ca216e6965e4ec183a1b3cb1cf3
Hari Gopal <mail@harigopal.in>
2018-06-02 21:55:35 +0530

    Add session query

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -7,7 +7,8 @@ class GraphqlController < ApiController
     operation_name = params[:operationName]
     context = {
       current_user: current_user,
-      current_session: current_session
+      current_session: current_session,
+      auth_token: auth_token
     }
     result = TurakuSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,8 +3,15 @@ class Types::QueryType < Types::BaseObject
     argument :email, String, required: true
   end
 
+  field :session, Types::Session, description: "Get current session", null: false
+
   def user(**args)
     user = User.find_by(email: args[:email])
     user.present? ? user : Users::StandInService.new(args[:email]).user
+  end
+
+  def session
+    # The authentication token needs to be taken from the request because we don't store it server-side.
+    context[:current_session].tap { |s| s.token = context[:auth_token] }
   end
 end
