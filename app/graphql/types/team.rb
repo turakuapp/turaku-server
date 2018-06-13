1802064df0b0346d795a0b7f64d3d134f11415e0
Hari Gopal <mail@harigopal.in>
2018-06-13 16:46:08 +0530

    Allow fetching of a team's users

--- app/graphql/types/team.rb
+++ app/graphql/types/team.rb
@@ -3,6 +3,7 @@ class Types::Team < Types::BaseObject
   field :name, String, null: false
   field :encrypted_password, Types::EncryptedData, null: false
   field :entries, [Types::Entry], null: false
+  field :users, [Types::User], null: false
 
   def encrypted_password
     object.team_memberships.find_by(user: context[:current_user]).encrypted_password
