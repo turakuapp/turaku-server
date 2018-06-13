3f064945f5be839b63b7ea410a5f59cebcbc6415
Hari Gopal <mail@harigopal.in>
2018-06-13 13:10:24 +0530

    Seed three entries for test team

--- db/seeds/development/entries.seeds.rb
+++ db/seeds/development/entries.seeds.rb
@@ -4,20 +4,24 @@ after 'development:teams' do
   puts 'Seeding entries...'
 
   john = User.find_by(email: 'johndoe@example.com')
-
-  entry_data = {
-    title: "Sample Entry #1",
-    fields: [
-      { kind: 'text', key: 'URL', value: 'https://example.com' },
-      { kind: 'password', key: 'Password', value: 'let me in' }
-    ]
-  }
-
   team = john.teams.first
 
-  Entry.create!(
-    user: john,
-    team: team,
-    encrypted_data: Seed.encryptData(john, team, 'password', entry_data.to_json)
-  )
+  def new_fake_entry
+    name = Faker::Company.name
+    {
+      title: name,
+      fields: [
+        { kind: 'text', key: 'URL', value: Faker::Internet.url(name) },
+        { kind: 'password', key: 'Password', value: SecureRandom.alphanumeric(16) }
+      ]
+    }
+  end
+
+  3.times do
+    Entry.create!(
+      user: john,
+      team: team,
+      encrypted_data: Seed.encryptData(john, team, 'password', new_fake_entry.to_json)
+    )
+  end
 end
