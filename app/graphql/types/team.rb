4c74a5b8f92eba5cbdfbc13f3519dca8b22557b4
Hari Gopal <mail@harigopal.in>
2018-06-17 18:22:33 +0530

    Allow querying of team's invitations

--- app/graphql/types/team.rb
+++ app/graphql/types/team.rb
@@ -5,6 +5,7 @@ class Types::Team < Types::BaseObject
   field :entries, [Types::Entry], null: false
   field :users, [Types::User], null: false
   field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+  field :invitations, [Types::Invitation], null: false
 
   def encrypted_password
     object.team_memberships.find_by(user: context[:current_user]).encrypted_password
