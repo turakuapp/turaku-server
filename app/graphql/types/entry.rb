c0e5836172f3cdecd0d2e26d5665ae2fff95cf1a
Hari Gopal <mail@harigopal.in>
2018-07-08 15:25:06 +0530

    Tag IDs for an entry should be queriable

--- app/graphql/types/entry.rb
+++ app/graphql/types/entry.rb
@@ -1,9 +1,13 @@
 class Types::Entry < Types::BaseObject
   field :id, ID, null: false
-  field :tags, [Types::Tag], null: false
+  field :tag_ids, [ID], null: false
   field :encrypted_data, Types::EncryptedData, null: false
   field :team, Types::Team, null: false
   field :user, Types::User, null: false
   field :created_at, GraphQL::Types::ISO8601DateTime, null: false
   field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+  def tag_ids
+    object.tags.pluck(:id)
+  end
 end
