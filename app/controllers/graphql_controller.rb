c296e57dca2a351875de92acb01f5ebbda0a1fbf
Hari Gopal <mail@harigopal.in>
2018-11-13 19:39:33 +0530

    Restore methods required for authentication from deleted ApiController

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,20 +1,37 @@
-class GraphqlController < ApiController
-  skip_before_action :authenticate_user_with_auth_token
+class GraphqlController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def execute
     variables = ensure_hash(params[:variables])
     query = params[:query]
     operation_name = params[:operationName]
+
     context = {
       current_user: current_user,
       current_session: current_session,
       auth_token: auth_token
     }
+
     result = TurakuSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+
     render json: result
   end
 
   private
+
+  def auth_token
+    return if request.headers['Authorization'].blank?
+
+    request.headers['Authorization'].split(' ').last
+  end
+
+  def current_session
+    @current_session ||= Sessions::AuthenticationService.new(auth_token).authenticate
+  end
+
+  def current_user
+    @current_user ||= current_session&.user
+  end
 
   # Handle form data, JSON body, or a blank value
   def ensure_hash(ambiguous_param)
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
   end
 
   post '/graphql', to: 'graphql#execute'
 
-  devise_for :users
   get 'home/index'
 
   root to: 'home#index'
