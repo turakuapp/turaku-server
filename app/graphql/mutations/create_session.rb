1cbb6bc5dfba67de489fc1d3e5a3534219318f6e
Hari Gopal <mail@harigopal.in>
2019-04-12 01:08:32 +0530

    Include teams and incoming invitations field after creating session

--- app/graphql/mutations/create_session.rb
+++ app/graphql/mutations/create_session.rb
@@ -5,6 +5,8 @@ class Mutations::CreateSession < Mutations::BaseMutation
   description "Create a new session for a user. The supplied password must have been salted and hashed with the user's authentication salt."
 
   field :session, Types::Session, null: true
+  field :teams, [Types::Team], null: false
+  field :incomingInvitations, [Types::IncomingInvitation], null: false
   field :errors, [Types::CreateSessionError], null: false
 
   def resolve(params)
@@ -16,9 +18,19 @@ class Mutations::CreateSession < Mutations::BaseMutation
       # Update the context now that the user has signed in.
       context[:current_user] = session.user
 
-      { session: session, errors: [] }
+      {
+        session: session,
+        teams: session.user.teams,
+        incoming_invitations: session.user.incoming_invitations,
+        errors: []
+      }
     else
-      { session: nil, errors: mutator.error_codes }
+      {
+        session: nil,
+        teams: [],
+        incoming_invitations: [],
+        errors: mutator.error_codes
+      }
     end
   end
 end
